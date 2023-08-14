6a2903b718ac27c46bde9200e7c72f6f628f7802
Albert Colubi <albertmc89@gmail.com>
2023-08-14 18:27:05 +0200

    Change handler import URL

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,7 +2,7 @@ import { rest } from "msw";
 import { filmsMock } from "./films";
 
 export const handlers = [
-  rest.get(`${import.meta.env.VITE_API_FILMS_URL}`, (_req, res, ctx) => {
+  rest.get(import.meta.env.VITE_API_FILMS_URL, (_req, res, ctx) => {
     return res(ctx.status(200), ctx.json(filmsMock));
   }),
 ];
