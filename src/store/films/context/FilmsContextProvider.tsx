b5a39f38b125f536f9c24d3a1e94c0c44bf6942d
Albert Colubi <albertmc89@gmail.com>
2023-08-14 16:31:09 +0200

    Add filmsContextValue to FilmsContextProvider

--- src/store/films/context/FilmsContextProvider.tsx
+++ src/store/films/context/FilmsContextProvider.tsx
@@ -1,9 +1,24 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useMemo, useState } from "react";
+import { Film } from "../../../types";
+import FilmsContext from "./FilmsContext";
+import { FilmsContextStructure } from "./types";
 
 const FilmsContextProvider = ({
   children,
 }: PropsWithChildren): React.ReactElement => {
-  return <FilmsContextProvider>{children}</FilmsContextProvider>;
+  const [films] = useState<Film[]>([]);
+
+  const filmsContextValue = useMemo(
+    (): FilmsContextStructure => ({
+      films,
+    }),
+    [films],
+  );
+  return (
+    <FilmsContext.Provider value={filmsContextValue}>
+      {children}
+    </FilmsContext.Provider>
+  );
 };
 
 export default FilmsContextProvider;
