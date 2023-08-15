fd68152646845d220bb9909acb7c0a88f220833a
Albert Colubi <albertmc89@gmail.com>
2023-08-15 09:38:21 +0200

    Add props in FilmCard and iterate films in FilmsList

--- src/components/FilmsList/FilmsList.tsx
+++ src/components/FilmsList/FilmsList.tsx
@@ -1,21 +1,18 @@
+import { useContext } from "react";
+import FilmsContext from "../../store/films/context/FilmsContext";
 import FilmCard from "../FilmCard/FilmCard";
 import "./FilmsList.css";
 
 const FilmsList = (): React.ReactElement => {
+  const { films } = useContext(FilmsContext);
+
   return (
     <ul className="films">
-      <li>
-        <FilmCard />
-      </li>
-      <li>
-        <FilmCard />
-      </li>
-      <li>
-        <FilmCard />
-      </li>
-      <li>
-        <FilmCard />
-      </li>
+      {films.map((film) => (
+        <li key={film.id}>
+          <FilmCard films={film} />
+        </li>
+      ))}
     </ul>
   );
 };
