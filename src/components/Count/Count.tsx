8a31fe412fd309291d66ac0f18c1a82355dfc27b
Albert Colubi <albertmc89@gmail.com>
2023-08-15 11:53:04 +0200

    Add count films

--- src/components/Count/Count.tsx
+++ src/components/Count/Count.tsx
@@ -1,5 +1,9 @@
+import { useContext } from "react";
+import FilmsContext from "../../store/films/context/FilmsContext";
+
 const Count = (): React.ReactElement => {
-  return <span className="count">Listando 4 películas</span>;
+  const { films } = useContext(FilmsContext);
+  return <span className="count">Listando {films.length} películas</span>;
 };
 
 export default Count;
