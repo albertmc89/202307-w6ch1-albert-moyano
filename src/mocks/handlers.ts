59418a08a90b8f2231c2bd7d1ed2aaea3e518dd1
Jose Manuel Vera <josemanuelvera1991@gmail.com>
2023-08-16 12:06:06 +0200

    Fix a refactor of handlers

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -5,21 +5,15 @@ export const handlers = [
   rest.get(import.meta.env.VITE_API_FILMS_URL, (_req, res, ctx) => {
     return res(ctx.status(200), ctx.json(filmsMock));
   }),
+  rest.post(import.meta.env.VITE_API_FILMS_URL, (_req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(newFilmMock));
+  }),
 ];
 
 export const handlersError = [
   rest.get(import.meta.env.VITE_API_FILMS_URL, (_req, res, ctx) => {
     return res(ctx.status(404, "Films couldn't be loaded"));
   }),
-];
-
-export const postHandlers = [
-  rest.post(import.meta.env.VITE_API_FILMS_URL, (_req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(newFilmMock));
-  }),
-];
-
-export const postHandlersError = [
   rest.post(import.meta.env.VITE_API_FILMS_URL, (_req, res, ctx) => {
     return res(ctx.status(404, "Films couldn't be created"));
   }),
